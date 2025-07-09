8f1e704895c7ee3ec1ded78246d7a07b746aaf60
Cylix Lee <cylix.lee@outlook.com>
2025-07-09 17:17:22 +0800

    feature: Add external Git dependency through Meson Wrap.

--- src/main.cc
+++ src/main.cc
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <sample-cpp-lib/greeting.h>
 #include <sample-subproject/version.h>
 
 int main(int argc, const char *argv[]) {
   const auto version = sample_subproject::get_version();
   std::cout << "sample-subproject Version: " << version.major << "."
             << version.minor << "." << version.patch << std::endl;
+  std::cout << sample_cpp_lib::greeting() << std::endl;
   return 0;
 }
+
+// To run this project, execute:
+//
+// `meson setup <builddir>`
+//
+// `meson compile -C <builddir>`
+//
+// and run the executable file compiled in <builddir>.
